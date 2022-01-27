9a9fb5108c909e706256083d59658810f29c4a65
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2022-01-27 23:13:25 +0100

    Lägger till metod för att skapa bloggpost

--- script/blogpost.js
+++ script/blogpost.js
@@ -1,5 +1,49 @@
-//Lägg till export class...
+/**
+ * Denna klass skapar listan med bloggposter samt innehåller
+ * metoder som skapar blogginlägg och plockar bort dem. FYLL PÅ!!!
+ */
+export class BlogPost {
+  /**
+   * Konstruktor som skapar en lista med 3 blogginlägg från start
+   * @constructor
+   * @param
+   *
+   */
+  costructor() {
+    this.post = [
+      {
+        image: (image.src = "../images/prag.jpeg"),
+        title: "Frukost i Prag",
+        date: Date.now(),
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+        author: "MissLi",
+      },
+      {
+        image: (image.src = "../images/prag.jpeg"),
+        title: "Frukost i Prag",
+        date: Date.now(),
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+        author: "MissLi",
+      },
+      {
+        image: (image.src = "../images/prag.jpeg"),
+        title: "Frukost i Prag",
+        date: Date.now(),
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+        author: "MissLi",
+      },
+    ];
+  }
 
-class BlogPost {
-  costructor() {}
+  addNewPost(givenImage, givenTitle, givenText, givenAuthor) {
+    const newPost = {
+      image: givenImage,
+      title: givenTitle,
+      date: Date.now(),
+      text: givenText,
+      author: givenAuthor,
+    };
+
+    this.post.push(newPost);
+  }
 }
